aa9f5fddd13f7c903e32d2ae870cab286be80b24
Avijit Ash <avijitash06@gmail.com>
2025-06-11 23:55:35 +0530

    add login path

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 
