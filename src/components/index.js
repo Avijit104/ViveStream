7809cc8793e68013b1a5f161671fbaac3a0c5ad6
Avijit Ash <avijitash06@gmail.com>
2025-06-11 20:17:29 +0530

    create login component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import SideNav from "./SideNav/SideNav";
-export { Header, Footer, SideNav };
+import Login from "./Login/Login";
+export { Header, Footer, SideNav, Login };
