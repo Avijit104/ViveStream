7fc89fce84f632097c6a8718500fe3d56435f051
Avijit Ash <avijitash06@gmail.com>
2025-06-13 22:56:09 +0530

    change file location of login component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import SideNav from "./SideNav/SideNav";
-import Login from "./Login/Login";
-export { Header, Footer, SideNav, Login };
+import Login from "./Authentication/Login/Login";
+import Signup from "./Authentication/SignUp/Signup";
+export { Header, Footer, SideNav, Login, Signup };
