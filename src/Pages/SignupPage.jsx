b1984995eeeaacc8c19859c40cee2b283787d9b9
Avijit Ash <avijitash06@gmail.com>
2025-06-14 17:16:40 +0530

    create signup Pages

---
+++ src/Pages/SignupPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Signup } from "../components";
+
+function SignupPage() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        background: "linear-gradient(to bottom, #1f1f1f 10%, transparent)",
+        height: "fit-content",
+        padding: "30px 0px",
+        boxSizing: "border-box",
+      }}
+    >
+      <Signup />
+    </div>
+  );
+}
+
+export default SignupPage;
